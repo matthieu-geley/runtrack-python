1914cc408ca5581979fbd87f61d2eb465e2374f8
Matthieu <115145423+matthieu-geley@users.noreply.github.com>
2023-01-03 15:29:45 +0100

    Ajout vérification de l'égalité du triangle rectangle

--- jour02/Pour aller plus loin/Job09.py
+++ jour02/Pour aller plus loin/Job09.py
@@ -1,7 +1,7 @@
 def triangle(a, b, c):
 
     if a+b > c and a+c > c and b+c > a:
-        if a*a+b*b == c*c:
+        if a*a+b*b == c*c or a ** 2 + c ** 2 == b ** 2 or a **2 == b ** 2 + c ** 2:
             print("triangle rectangle")
             if a == b or b == c or a == c:
                 print("triangle rectangle isocèle")
