15465458cb61e0faa532078181c48e1a8515ab24
Matthieu <115145423+matthieu-geley@users.noreply.github.com>
2023-01-06 09:56:51 +0100

    finis job 13 + début job 14

--- jour04/Job13/Job13.py
+++ jour04/Job13/Job13.py
@@ -2,17 +2,16 @@ def doublon():
     list = [10,20,30,20,10,50,60,40,80,50,40]
     n = 0
     list2=[]
+
     for i in list:
+       if i not in list2:
+           list2 += [i]
+    for i in list2:
         n += 1
     for i in range(0, n):
-        for j in range(i, n):
-            if list[i] > list[j]:
-                list[i], list[j] = list[j], list[i]
-        for i in range(0, n):
             for j in range(i, n):
-                if list[i] == list[j]:
-                    list[i] = list2
-        print(list)
+                if list2[i] > list2[j]:
+                    list2[i], list2[j] = list2[j], list2[i]
     print(list2)
 
 doublon()
