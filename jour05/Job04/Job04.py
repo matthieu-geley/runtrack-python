abff4a9ce9d9f7eb05493381eb4adf256a6bd981
Matthieu <115145423+matthieu-geley@users.noreply.github.com>
2023-01-09 08:36:18 +0100

    Mise à jour Job04
    Modification et finition du Job04 du jour 5

--- jour05/Job04/Job04.py
+++ jour05/Job04/Job04.py
@@ -1,15 +1,16 @@
 def chiffrement(str):
-    lettres = [*str]
-    chifr = [*"abcdefghijklmnopqrstuvwxyz"]
+    split_str = ""
+    lettr = "abcdefghijklmnopqrstuvwxyz"
     i = 0
-    rangement=[]
-    for j in lettres:
-        lettres[i] = chifr[(i+3) % 26]
-        i += 1
-    print(lettres)
-    print(chifr)
-    for i in str:
-        chifr.index(i)
-        i+=1
+    while i < len(str):
+        j = 0
+        while j < len(lettr):
+            if str[i].lower() == lettr[j]:
+                split_str += lettr[(j + 3) % len(lettr)]
+            j += 1
 
-chiffrement("Luke Skywalker un professeur de Math fait passer un test et decide")
+        i += 1
+    print(split_str)
+
+chiffrement("abcdefghijklmnopqrstuvwxyz")
+chiffrement("ceci est un message sous chiffrage")
